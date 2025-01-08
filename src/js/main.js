7c4b553b5db81d30678a2c1503e6e292213abadf
Taras <taraslebedunsky@gmail.com>
2025-01-08 15:18:57 +0200

    Fix Bag

--- src/js/main.js
+++ src/js/main.js
@@ -246,16 +246,26 @@ window.addEventListener('DOMContentLoaded', function() {
             request.open('POST', 'server.php');
 
             
-            // request.setRequestHeader('Content-type', 'multipart/form-data');
-
+            request.setRequestHeader('Content-type', 'application/json');
             const formData = new FormData(form);
 
-            request.send(formData);
+            const object = {};
+            formData.forEach((value, key)=> {
+                object[key] = value;
+            });
+
+            const json = JSON.stringify(object)
+
+            request.send(json);
 
             request.addEventListener('load', ()=> {
                 if(request.status === 200) {
                     console.log(request.response);
                     statusMessage.textContent = massage.success;
+                    form.reset();
+                    setTimeout(()=> {
+                        statusMessage.remove();
+                    }, 2000);
                 } else {
                     statusMessage.textContent = massage.failer;
                 }
